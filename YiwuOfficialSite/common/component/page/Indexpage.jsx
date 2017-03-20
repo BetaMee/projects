9227c46c3cbea97c4ec8fa9b1a7b190b728872f6
betamee <gongxq95@gmail.com>
2017-03-20 21:47:02 +0800

    更改

--- YiwuOfficialSite/common/component/page/Indexpage.jsx
+++ YiwuOfficialSite/common/component/page/Indexpage.jsx
@@ -2,11 +2,6 @@ import React, { Component } from 'react';
 import classNames from 'classnames';
 import CSSStyles from './Indexpage.css';
 
-const svg={
-  stroke:"#006600", 
-  fill:"#00cc00",
-}
-
 class Indexpage extends Component {
   componentDidMount() {
     console.log('indexpage');
@@ -16,9 +11,7 @@ class Indexpage extends Component {
   render() {
     return (
       <div className={classNames(CSSStyles.container, CSSStyles.animated)}>
-        <svg>
-          <circle cx="40" cy="40" r="24" style={svg}/>
-         </svg>
+      
       </div>
     );
   }
