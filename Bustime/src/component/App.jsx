3b6cc1e625e7509c42c95b1dbc29fa37f14feb07
betamee <gongxq95@gmail.com>
2017-03-23 00:29:34 +0800

    改用react重写

---
+++ Bustime/src/component/App.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+import JlCarLine from './JlCarLine';
+import JlTimeLine from './JlTimeLine';
+import XpCarLine from './XpCarLine';
+import XpTimeLine from './XpTimeLine';
+
+import CSSStyles from './App.css';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  componentDidMount() {
+    
+  }
+  
+  render() {
+    return (
+      <div className={CSSStyles.Background}>
+        <XpTimeLine />
+        <JlTimeLine />
+        <XpCarLine />
+        <JlCarLine />
+      </div>
+    );
+  }
+}
+
