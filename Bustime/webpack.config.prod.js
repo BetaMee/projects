45d9dccc6ec41b93e32db9af6a91799c84a56e31
betamee <gongxq95@gmail.com>
2017-03-27 01:14:24 +0800

    生产环境重打包

--- Bustime/webpack.config.prod.js
+++ Bustime/webpack.config.prod.js
@@ -81,17 +81,17 @@ module.exports={
   },
 
   plugins:[
-    // new webpack.DefinePlugin({
-		// 	'process.env': {
-		// 		NODE_ENV: JSON.stringify('production'),
-		// 		WEBPACK: true
-		// 	}
-		// }),
-		// new webpack.optimize.UglifyJsPlugin({
-		// 	compressor: {
-		// 		warnings: false
-		// 	}
-		// }),
+    new webpack.DefinePlugin({
+    	'process.env': {
+    		NODE_ENV: JSON.stringify('production'),
+    		WEBPACK: true
+    	}
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+    	compressor: {
+    		warnings: false
+    	}
+    }),
     new ExtractTextPlugin('bundle.css')
   ]
 }
