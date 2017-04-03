bb0466ab4bf5052e738459beb416ba8e095731fb
betamee <gongxq95@gmail.com>
2017-04-04 01:32:18 +0800

    完成首部

--- YiwuOfficialSite/common/component/page/portion/Part1.jsx
+++ YiwuOfficialSite/common/component/page/portion/Part1.jsx
@@ -7,17 +7,26 @@ function Part1() {
   return (
     <section className={CSSStyles.container}>
       <header className={CSSStyles.header}>
-        <div>
+        <div className={CSSStyles.table}>
           <div>
             Logo
           </div>
           <div>
-
+            fllow us
           </div>
         </div>
       </header>
-      <div>
-      
+      <div className={CSSStyles.intro}>
+          <div className={CSSStyles.slogan}>
+            <span>这是一款全新的产品</span>
+          </div>
+          <div className={CSSStyles.buttom}>
+            <a href="#" className={CSSStyles.b1}>现在下载</a>
+            <a className={CSSStyles.b2}>查看证书</a>          
+          </div>
+          <span> 
+            现在可在安卓市场上下载
+          </span>
       </div>
 
       <div>
