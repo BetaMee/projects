0a7e8be9795f1b4439fd1ca5e02c5e1d08ba92de
betamee <gongxq95@gmail.com>
2017-04-06 22:53:52 +0800

    更新脚本

--- YiwuOfficialSite/config/development.js
+++ YiwuOfficialSite/config/development.js
@@ -3,7 +3,7 @@ module.exports = {
   session: {
     secret: 'react',
     key: 'react',
-    maxAge: 2592000000
+    maxAge: 2592000000,
   },
-  mongodb: 'mongodb://localhost:27017/react'
+  mongodb: 'mongodb://localhost:27017/react',
 };
