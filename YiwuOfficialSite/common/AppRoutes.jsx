a9d836a8ceb689146b15443d7cfaa1db42c3b6e9
betamee <gongxq95@gmail.com>
2017-04-09 22:49:10 +0800

    重新打包

--- YiwuOfficialSite/common/AppRoutes.jsx
+++ YiwuOfficialSite/common/AppRoutes.jsx
@@ -3,13 +3,12 @@ import { Route, IndexRoute } from 'react-router';
 
 import Layout from './component/Layout';
 import Indexpage from './component/page/Indexpage'; // 首页
-import NotFoundPage from './component/page/NotFoundPage'; // 错误页
+// import NotFoundPage from './component/page/NotFoundPage'; // 错误页
 
 
 const AppRoutes = (
   <Route path="/" component={Layout}>
     <IndexRoute component={Indexpage} />
-    <Route path="*" component={NotFoundPage} />
   </Route>
 );
 
