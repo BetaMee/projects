7dd4f6812dfa12ee067c0646e4d830b564457dc4
betamee <gongxq95@gmail.com>
2017-04-09 23:10:04 +0800

    使用hashHistory，纯资源引用不能使用browserhistory

--- YiwuOfficialSite/client/App-Client.jsx
+++ YiwuOfficialSite/client/App-Client.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // 路由管理
-import { Router, browserHistory } from 'react-router';
+import { Router, hashHistory } from 'react-router';
 import AppRoutes from '../common/AppRoutes';
 // 状态管理
 
 
 function Root() {
   return (
-    <Router history={browserHistory}>
+    <Router history={hashHistory}>
       {AppRoutes}
     </Router>
   );
